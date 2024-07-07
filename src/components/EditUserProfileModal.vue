92aea56a55e690815f1d1c656b8598a7ba872716
Ângelo Cardoso <hoanngelos@gmail.com>
2024-07-06 21:21:04 -0300

    edit user modal added to flow.

---
+++ src/components/EditUserProfileModal.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="main-modal fixed w-full h-100 inset-0 z-50 overflow-hidden flex justify-center items-center animated fadeIn faster">
+    <div class="border border-gray-900 modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+      <div class="modal-content py-4 text-left px-6">
+        <div class="flex justify-between items-center pb-3">
+          <p class="text-2xl font-bold">Editar Usuário</p>
+          <div @click="close" class="modal-close cursor-pointer z-50">
+            <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+              <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z">
+              </path>
+            </svg>
+          </div>
+        </div>
+        <div class="my-5 flex flex-col space-y-4">
+          <input
+            v-model="editUserData.name"
+            type="text"
+            placeholder="nome"
+            class="py-1 px-4 border border-gray-400 focus:outline-none rounded-md focus:ring-1 ring-slate-500"
+          />
+          <input
+            v-model="editUserData.nickname"
+            type="text"
+            placeholder="apelido"
+            class="mb-3 py-1 px-4 border border-gray-400 focus:outline-none rounded-md focus:ring-1 ring-slate-500"
+          />
+          <input
+            v-model="editUserData.email"
+            type="text"
+            placeholder="email"
+            class="mb-3 py-1 px-4 border border-gray-400 focus:outline-none rounded-md focus:ring-1 ring-slate-500"
+          />
+          <input
+            v-model="editUserData.password"
+            type="password"
+            placeholder="senha"
+            class="mb-3 py-1 px-4 border border-gray-400 focus:outline-none rounded-md focus:ring-1 ring-slate-500"
+          />
+          <div class="flex justify-end pt-2">
+            <button @click="close" class="focus:outline-none modal-close px-4 border border-black p-3 rounded-lg text-black hover:bg-yellow-400">
+              Cancelar
+            </button>
+            <input type="submit" value="Criar" class="cursor-pointer focus:outline-none px-4 bg-white p-3 ml-3 border border-black rounded-lg text-black hover:bg-yellow-400 hover:text-black">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditUserProfileModal',
+  data() {
+    return {
+      editUserData: {
+        name: '',
+        nickname: '',
+        email: '',
+        password: '',
+      }
+    }
+  },
+  computed: {},
+  methods: {
+    close() {
+      this.$emit('close');
+    }
+  },
+}
+</script>
+
+<style>
+.note-content {
+  resize: none;
+}
+
+.main-modal {
+  background: #000000b3;
+}
+</style>
--- src/views/UserInfoView.vue
+++ src/views/UserInfoView.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="bg-slate-200 fixed w-full h-100 inset-0 z-50 overflow-hidden flex justify-center items-center animated fadeIn faster">
+  <EditUserProfileModal v-if="showEditUserProfileModal" @close="close" />
+  <div v-else class="bg-slate-200 fixed w-full h-100 inset-0 z-50 overflow-hidden flex justify-center items-center animated fadeIn faster">
     <div class="bg-white w-11/12 h-[80%] md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
       <div class="py-4 text-left px-6 h-full flex flex-col justify-between">
         <div class="flex justify-between items-center pb-3">
@@ -7,9 +8,9 @@
         </div>
         <div class="my-5 text-2xl flex flex-col h-96 space-y-4">
           <p><b>ID:</b> {{ user.id }}</p>
-            <p><b>Nome:</b> {{ user.name }}</p>
-            <p><b>Apelido:</b> {{ user.nickname }}</p>
-            <p><b>E-mail:</b> {{ user.email }}</p>
+          <p><b>Nome:</b> {{ user.name }}</p>
+          <p><b>Apelido:</b> {{ user.nickname }}</p>
+          <p><b>E-mail:</b> {{ user.email }}</p>
         </div>
         <div class="flex justify-between pt-2">
           <router-link to="/dashboard"
@@ -18,6 +19,7 @@
           </router-link>
 
           <button
+            @click="triggerEditUserProfileModal"
             class="cursor-pointer focus:outline-none px-4 bg-white p-3 ml-3 border border-black rounded-lg text-black hover:bg-yellow-400 hover:text-black">
             Editar perfil
           </button>
@@ -29,11 +31,27 @@
 
 <script>
 import { mapState } from 'vuex';
+import EditUserProfileModal from '@/components/EditUserProfileModal.vue';
 
 export default {
   name: 'UserInfoView',
+  components: { EditUserProfileModal },
+  data() {
+    return {
+      showEditUserProfileModal: false,
+    }
+  },
   computed: {
     ...mapState(['user'])
+  },
+  methods: {
+    triggerEditUserProfileModal() {
+      this.showEditUserProfileModal = true;
+    },
+
+    close() {
+      this.showEditUserProfileModal = false;
+    }
   },
 }
 </script>
